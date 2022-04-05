9c5759b669243281fffdf5ea0a63721c456ce4b6
mensch <mensch25@yandex.ru>
2022-04-05 22:53:46 +0300

    recolor buttons & add rgb indicator

--- app/src/main/java/com/dol/plants/RecognizeActivity.kt
+++ app/src/main/java/com/dol/plants/RecognizeActivity.kt
@@ -1,8 +1,13 @@
 package com.dol.plants
 
 import android.content.Intent
+import android.graphics.Color.WHITE
+import android.graphics.Color as AColor
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.appcompat.app.AppCompatActivity
 import com.dol.plants.widget.ProgressSeekBar
 
@@ -23,19 +28,46 @@ class RecognizeActivity : AppCompatActivity() {
         val width = findViewById<ProgressSeekBar>(R.id.width)
         val length = findViewById<ProgressSeekBar>(R.id.length)
 
-        findViewById<Button>(R.id.white_color).setOnClickListener {
+        val whiteButton = findViewById<Button>(R.id.white_color)
+        val greenButton = findViewById<Button>(R.id.green_color)
+        val deepGreenButton = findViewById<Button>(R.id.deep_green_color)
+
+        val rgbIndicator = findViewById<View>(R.id.rgb)
+        rgbIndicator.setBackgroundColor(AColor.rgb(red.progress, green.progress, blue.progress))
+
+        val changeRgbIndicator = object: OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar?, start: Int, end: Boolean) {
+                rgbIndicator.setBackgroundColor(AColor.rgb(red.progress, green.progress, blue.progress))
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                rgbIndicator.setBackgroundColor(AColor.rgb(red.progress, green.progress, blue.progress))
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                rgbIndicator.setBackgroundColor(AColor.rgb(red.progress, green.progress, blue.progress))
+            }
+        }
+        red.setOnSeekBarChangeListener(changeRgbIndicator)
+        green.setOnSeekBarChangeListener(changeRgbIndicator)
+        blue.setOnSeekBarChangeListener(changeRgbIndicator)
+        whiteButton.setBackgroundColor(WHITE)
+        whiteButton.setOnClickListener {
             red.progress = 255
             green.progress = 255
             blue.progress = 255
         }
 
-        findViewById<Button>(R.id.green_color).setOnClickListener {
+        greenButton.setBackgroundColor(AColor.rgb(0, 230, 0))
+        greenButton.setOnClickListener {
             red.progress = 0
             green.progress = 230
             blue.progress = 0
         }
 
-        findViewById<Button>(R.id.deep_green_color).setOnClickListener {
+        deepGreenButton.setBackgroundColor(AColor.rgb(0, 255, 0))
+        deepGreenButton.setOnClickListener {
             red.progress = 0
             green.progress = 255
             blue.progress = 0
